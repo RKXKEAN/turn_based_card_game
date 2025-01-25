3a67b8433ce6e2dabded30d96faead4f29318545
PATTARAPONG KHAOKHAIKAEW <roykaen.30@gmail.com>
2025-01-25 11:29:27 +0700

    Update HP value

--- main.py
+++ main.py
@@ -85,8 +85,8 @@ class TurnBasedCardGame(BoxLayout):
         self.go_to_main_menu_callback = go_to_main_menu_callback
 
         # Initialize attributes
-        self.player_hp = 200
-        self.enemy_hp = 200
+        self.player_hp = 100
+        self.enemy_hp = 100
         self.player_defense = 0
         self.enemy_defense = 0
         self.enemy_attack_debuff = 0
